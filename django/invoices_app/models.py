d965ffd9524609c8c82dd5bba1eaf12664c3c6b8
Marko Mudrinić <mudrinic.mare@gmail.com>
2021-01-23 18:15:16 +0100

    Manually manage database
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- django/invoices_app/models.py
+++ django/invoices_app/models.py
@@ -1,87 +1,94 @@
-import django.utils.timezone
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-from django.contrib.auth.models import User
 
-
-# Model PTT brojevi
 
 class City(models.Model):
+    id = models.IntegerField(primary_key=True)
     post_code = models.CharField(max_length=5, db_index=True, unique=True)
     city = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.post_code + ' ' + self.city
+    class Meta:
+        managed = False
+        db_table = 'sys_city'
 
-
-# Model korisnik sistema
 
 class MyCompany(models.Model):
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
-    address = models.CharField(max_length=50)
-    city = models.ForeignKey(City, on_delete=models.RESTRICT)
     tax_number = models.CharField(max_length=9)
     national_id = models.CharField(max_length=13)
-    email = models.CharField(max_length=50)
-    payment_account = models.CharField(max_length=25, blank=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
-# Model komintenta
-
-class Company(models.Model):
-    number = models.CharField(max_length=4, db_index=True, unique=True)
-    name = models.CharField(max_length=50)
     address = models.CharField(max_length=50)
-    city = models.ForeignKey(City, on_delete=models.RESTRICT)
-    tax_number = models.CharField(max_length=9)
-    national_id = models.CharField(max_length=13)
+    city = models.ForeignKey(City, models.RESTRICT)
     email = models.CharField(max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True)
+    payment_account = models.CharField(max_length=25, blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # created_by = models.ForeignKey(User, on_delete=models.RESTRICT)
 
-    def __str__(self):
-        return self.name
+    class Meta:
+        managed = False
+        db_table = 'sys_company'
 
-
-# Model poreskih stopa
 
 class TaxRate(models.Model):
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=15)
     value = models.DecimalField(max_digits=4, decimal_places=2)
 
-    def __str__(self):
-        return self.name
+    class Meta:
+        managed = False
+        db_table = 'sys_tax_rate'
 
 
-# Model racuna
-
-class Invoice(models.Model):
-    number = models.CharField(max_length=5, db_index=True, unique=True)
-    date = models.DateField(default=django.utils.timezone.now)
-    date_from = models.DateField(default=django.utils.timezone.now)
-    date_to = models.DateField(default=django.utils.timezone.now)
-    company = models.ForeignKey(Company, on_delete=models.RESTRICT)
-    remarks = models.TextField(blank=True)
+class Company(models.Model):
+    id = models.IntegerField(primary_key=True)
+    number = models.CharField(max_length=4, db_index=True, unique=True)
+    name = models.CharField(max_length=50)
+    national_id = models.CharField(unique=True, max_length=13)
+    tax_number = models.CharField(unique=True, max_length=9)
+    address = models.CharField(max_length=50)
+    city = models.ForeignKey(City, models.RESTRICT)
+    email = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # created_by = models.ForeignKey(User, on_delete=models.RESTRICT)
 
-    def __str__(self):
-        return self.number
+    class Meta:
+        managed = False
+        db_table = 'company'
+
+
+class Invoice(models.Model):
+    id = models.IntegerField(primary_key=True)
+    number = models.CharField(max_length=5, db_index=True, unique=True)
+    date = models.DateTimeField(auto_now_add=True)
+    date_from = models.DateTimeField(auto_now_add=True)
+    date_to = models.DateTimeField(auto_now_add=True)
+    company = models.ForeignKey(Company, models.RESTRICT)
+    remarks = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        managed = False
+        db_table = 'invoice'
 
 
 class InvoiceItem(models.Model):
+    id = models.IntegerField(primary_key=True)
+    invoice = models.ForeignKey(Invoice, models.CASCADE)
     description = models.TextField()
     unit = models.CharField(max_length=4)
     count = models.DecimalField(max_digits=10, decimal_places=3)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    tax_rate = models.ForeignKey(TaxRate, on_delete=models.RESTRICT)
-    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    tax_rate = models.ForeignKey(TaxRate, models.RESTRICT)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # created_by = models.ForeignKey(User, on_delete=models.RESTRICT)
 
-    def __str__(self):
-        return self.description
+    class Meta:
+        managed = False
+        db_table = 'invoice_item'
