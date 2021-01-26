031a74bda63eb84f6c88f14c61bbd6a58ff0ee28
Marko Mudrinić <mudrinic.mare@gmail.com>
2021-01-26 01:00:51 +0100

    Added navbar
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- invoices-frontend/src/App.vue
+++ invoices-frontend/src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/invoice/new">New Invoice</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
--- invoices-frontend/src/components/Header.vue
+++ invoices-frontend/src/components/Header.vue
@@ -1,7 +1,26 @@
 <template>
   <div>
-    <b-jumbotron header="Invoices" :lead="'Welcome to the Invoices application.'">
-    </b-jumbotron>
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-nav>
+        <b-navbar-brand to="/">Invoices App</b-navbar-brand>
+
+        <b-nav-item-dropdown text="Invoices" to="/">
+          <b-dropdown-item to="/">Preview all invoices</b-dropdown-item>
+          <b-dropdown-item to="/invoice/new">New invoice</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown text="Companies" to="/">
+          <b-dropdown-item>Preview all companies</b-dropdown-item>
+          <b-dropdown-item>New company</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown text="System" to="/">
+          <b-dropdown-item>Tax rates</b-dropdown-item>
+          <b-dropdown-item>Cities</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-navbar>
+    <div class="mt-5"></div>
   </div>
 </template>
 
